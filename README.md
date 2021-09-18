21aa59618d869af1bf811b604e91e3ed0116bd61
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-18 11:12:28 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -11,10 +11,21 @@
 
 # Local
 ```
+$ git clone https://github.com/nekozu/fbdl-bot
+
+$ cd fbdl-bot
+
+$ npm i
+
+$ cp .env.sample .env
+
+Replace your token at .env
+
+$ node index
 ```
 
 # Railway
-
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Frailwayapp%2Fexamples%2Ftree%2Fmaster%2Fexamples%2Fflask)
 
 After the fork, don't forget to give it a star!
 
--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const bot = new Telegraf(process.env.token)
 const fbdl = require("fb-video-downloader")
 
 bot.command('start', (ctx) => {
-    ctx.reply(process.env.text);
-});
+    if (ctx.chat.type == 'private') {
+        ctx.reply(process.env.text)
+    }else{
+          ctx.reply(process.env.text_group)
+  }
+})
+
+
 bot.on('message',  (ctx) => {
     if (ctx.chat.type == 'private') {
         ctx.reply('Downloading..');
@@ -15,13 +21,13 @@ bot.on('message',  (ctx) => {
         // inputArray.shift()
         // pesan = inputArray.join(" ")
         fbdl.getInfo(pesan).then(res => {
-            //ctx.replyWithVideo(res.streamURL, { caption: `Title: ${res.title}`})
+            //ctx.replyWithVideo(res.streamURL, { caption: `Title: ${res.title}`})                                                 
             //ctx.replyWithPhoto({url: res.thumb}, { caption:  `Title: ${res.title}\nSize: ${res.size}\n[Click To Download](${res.download.sd})`, parse_mode: "markdown"})
             ctx.reply(`Title: ${res.title}\n[Click To Download](${res.download.sd})`, { parse_mode: 'markdown'})
         }).catch(e => {
          console.log(e);
    })
- }
-})
+  }
+ })
 
 bot.launch();
